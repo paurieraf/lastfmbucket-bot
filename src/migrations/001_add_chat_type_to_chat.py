e5e228d24d28c9c7af1b557ce0086bc9c624eb15
Pau Riera <pau.riera.forteza@gmail.com>
2026-01-18 00:31:54 +0100

    Use raw SQL for adding `chat_type` column to `Chat` and `chat_id` FK to `CommandLog` for SQLite compatibility in migration.

--- src/migrations/001_add_chat_type_to_chat.py
+++ src/migrations/001_add_chat_type_to_chat.py
@@ -1,22 +1,18 @@
 """
-Add chat_type column to Chat table and update CommandLog FK.
-
-Peewee migrations, see http://docs.peewee-orm.com/en/latest/peewee/playhouse.html#schema-migrations
+Add chat_type column to Chat table and chat FK to CommandLog.
 """
 
-from peewee import CharField, ForeignKeyField, BigIntegerField
+import peewee as pw
 
 
 def migrate(migrator, database, fake=False, **kwargs):
     """Add chat_type to Chat and chat FK to CommandLog."""
-    # Add chat_type to Chat table
-    migrator.add_column("chat", "chat_type", CharField(default=""))
-
-    # Add chat_id FK column to CommandLog (Peewee stores FK as <field>_id)
-    migrator.add_column("commandlog", "chat_id", BigIntegerField(null=True))
+    # Use raw SQL for SQLite compatibility
+    database.execute_sql('ALTER TABLE "chat" ADD COLUMN "chat_type" VARCHAR(255) DEFAULT ""')
+    database.execute_sql('ALTER TABLE "commandlog" ADD COLUMN "chat_id" INTEGER REFERENCES "chat" ("id")')
 
 
 def rollback(migrator, database, fake=False, **kwargs):
     """Rollback the migration."""
-    migrator.drop_column("chat", "chat_type")
-    migrator.drop_column("commandlog", "chat_id")
+    # SQLite doesn't support DROP COLUMN easily, would need to recreate table
+    pass
